7a35c3b232694447eaf02528ca322805fad4bfa8
codebox workspace user <madjake45@madjake45.com>
2014-09-15 12:23:20 +0000

    Messenger and protected

---
+++ projects.madjake45.com/protected/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$_CONFIG = array(
+    'SITE_NAME' => "madjake45's Projects",
+    'DEBUG' => true,
+    'DOMAIN' => 'http://www.madjake45.com',
+    'EMAIL' => 'madjake45@madjake45.com',
+    'SUPPORT_EMAIL' => 'support@madjake45.com',
+    'TWITTER_USERNAME' => 'madjake45',
+);
