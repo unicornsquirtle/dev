eded001b737010d786c39c986c77598da0b76d1a
madjake45 <madjake45@madjake45.com>
2014-10-06 19:07:20 +0100

    Correcting kjptheking so that it uses and array instead of a switch

--- kjptheking.info/index.php
+++ kjptheking.info/index.php
@@ -5,6 +5,21 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta name="author" content="madjake45"/>
         <title>Welcome</title>
+        <?php
+        $array = array(
+
+            "http://t3.gstatic.com/images?q=tbn:ANd9GcRpYwpxCRNt0SEVEffLHXe-VFmDDL1NV5J3fnzGoEK2OQYqoag6:www.howtodrawmanga3d.com/sites/default/files/imagecache/obrazok-do-tutorialu/user/tutorials/how_to_draw_magikarp_pokemon/how_draw_magikarp_pokemon.jpg",
+            "https://pbs.twimg.com/profile_images/443835622747549697/PSD-VNdL.jpeg",
+            "http://t3.gstatic.com/images?q=tbn:ANd9GcRuz7xWsnPRM7Ely0B9w5E-BBweHvv01989QChFrN5bXP__vloilQ:https://gp6.googleusercontent.com/-MbaEVoOeerk/AAAAAAAAAAI/AAAAAAAAAAA/n6z-m4TQaDg/s48-c-k-no/photo.jpg%3Fsz%3D50",
+            "http://kjptheking.info/kjp1.jpeg",
+            "http://kjptheking.info/kjp2.jpeg",
+            "http://kjptheking.info/kjp3.jpeg",
+
+        );
+
+        $img = $array[array_rand($array)];
+        ?>
+        <link rel="icon" href="<?php echo $img ?>" type="image/x-icon">
         
     </head>
     <body>
@@ -12,23 +27,8 @@
 
 
         <img src="<?php
-        switch (rand(1, 3)) {
-            case '1':
-              echo "http://t3.gstatic.com/images?q=tbn:ANd9GcRpYwpxCRNt0SEVEffLHXe-VFmDDL1NV5J3fnzGoEK2OQYqoag6:www.howtodrawmanga3d.com/sites/default/files/imagecache/obrazok-do-tutorialu/user/tutorials/how_to_draw_magikarp_pokemon/how_draw_magikarp_pokemon.jpg";
-                break;
-                
-            case '2';
-              echo "https://pbs.twimg.com/profile_images/443835622747549697/PSD-VNdL.jpeg";
-                break;
-                
-            case '3';
-              echo "http://t3.gstatic.com/images?q=tbn:ANd9GcRuz7xWsnPRM7Ely0B9w5E-BBweHvv01989QChFrN5bXP__vloilQ:https://gp6.googleusercontent.com/-MbaEVoOeerk/AAAAAAAAAAI/AAAAAAAAAAA/n6z-m4TQaDg/s48-c-k-no/photo.jpg%3Fsz%3D50";
-                break;
-            
-            default:
-                echo "http://t3.gstatic.com/images?q=tbn:ANd9GcRuz7xWsnPRM7Ely0B9w5E-BBweHvv01989QChFrN5bXP__vloilQ:https://gp6.googleusercontent.com/-MbaEVoOeerk/AAAAAAAAAAI/AAAAAAAAAAA/n6z-m4TQaDg/s48-c-k-no/photo.jpg%3Fsz%3D50";
-                break;
-        }
+
+        echo $img;
 
         ?>" alt="Kyle Pomroy"/>
 
